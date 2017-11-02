438ba6b56bff0654bd9e837a74e19539b6700e3c
Kucsvan Attila <attylax1@gmail.com>
2017-11-02 09:46:21 +0200

    Table_added
    Create User table and Create supplirers table

--- database/migrations/2017_10_26_063555_suppliers_table.php
+++ database/migrations/2017_10_26_063555_suppliers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class SuppliersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        //
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('Name');
+            $table->string('Location');
         });
     }
 
@@ -30,6 +28,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::drop('suppliers');
     }
 }
