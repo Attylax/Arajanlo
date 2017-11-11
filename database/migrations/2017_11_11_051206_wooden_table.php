f51d4663b3db83253ab501a10b5c0068690833a5
istvan73 <32622805+istvan73@users.noreply.github.com>
2017-11-11 10:38:59 +0200

    Update Timber to Wooden

--- database/migrations/2017_11_11_051206_wooden_table.php
+++ database/migrations/2017_11_11_051206_wooden_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TimberTable extends Migration
+class WoodenTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2017_11_11_051252_wooden_price_table.php
+++ database/migrations/2017_11_11_051252_wooden_price_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TimberPriceTable extends Migration
+class WoodenPriceTable extends Migration
 {
     /**
      * Run the migrations.
