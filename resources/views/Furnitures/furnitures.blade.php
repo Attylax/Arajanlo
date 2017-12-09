4563d833bee7e003f1d71d345fff370d0a4d7852
istvan73 <32622805+istvan73@users.noreply.github.com>
2017-12-09 12:40:22 +0200

    added dummy to furniture

--- resources/views/Furnitures/furnitures.blade.php
+++ resources/views/Furnitures/furnitures.blade.php
@@ -4,9 +4,19 @@
 @stop
 
 @section('css')
-    
+
 @stop
 
 @section('content')
-    A bútorok az szomorú mélytorok?
+    <tr>
+        <td>
+            <div class="furniture_table_element">
+                <img src="{{asset('Images/dummy_image.jpg')}}">
+                <h1>Item name</h1>
+                <p>Box short description</p>
+            </div>
+        </td>
+        <td></td>
+    </tr>
+
 @stop
